dddc9aaad0aadd68fca51bc8526f5f8474e758e8
Anukul <anukul.p@growthsourceft.com>
2022-01-24 18:21:12 +0530

    text validations

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,20 @@ import Card from './shared/Card';
 
 const FeedbackForm = () => {
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
   const handleTextChange = (event) => {
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10) {
+      setMessage('Text must be atleast 10 characters');
+      setBtnDisabled(false);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
     setText(event.target.value);
   };
   return (
@@ -20,8 +32,11 @@ const FeedbackForm = () => {
             placeholder="Write a review"
             value={text}
           />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
